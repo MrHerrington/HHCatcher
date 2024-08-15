bdca988d145191e460ba8eb105e6f0c28a50ba2a
Ilya Belov <154321709+MrHerrington@users.noreply.github.com>
2024-08-15 18:12:02 +0300

    Complete relevant info parser for solo page. Code review, optimization.

--- main.py
+++ main.py
@@ -1,60 +1,116 @@
+# -*- coding: utf-8 -*-
+
+
 import json
-from contextlib import contextmanager
 import logging
 import sys
+import typing as ty
+from contextlib import contextmanager
+from pathlib import Path
 from time import sleep
 
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC  # noqa
 from selenium.webdriver.support.ui import WebDriverWait
-from bs4 import BeautifulSoup
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - [%(levelname)s]: %(message)s",
-    stream=sys.stdout
-)
-logger = logging.getLogger(__name__)
+###################
+# Driver  section #
+###################
+def create_driver(headless: bool = True) -> webdriver.Chrome:
+    def get_options() -> webdriver.ChromeOptions:
+        options = webdriver.ChromeOptions()
 
-timeout = 10
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/"
+            "537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+        )
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--start-maximized")
+        if headless:
+            options.add_argument("--headless")
 
-with open('credentials.json') as file:
-    creds = json.load(file)
-    creds_login, creds_password = creds['credentials']['login'], creds['credentials']['password']
+        return options
 
-logger.info('Browser driver creation...')
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
-chrome_options.add_argument("--window-size=1920,1080")
-chrome_options.add_argument("--start-maximized")
-# chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options)
-logger.info('Browser driver is created.')
+    logger.info('Browser driver creation...')
+    chrome_options = get_options()
+    driver_ = webdriver.Chrome(options=chrome_options)
+    driver_.TIMEOUT = 10
+    driver_.DEFAULT_PAGE_LOAD_TIMEOUT = driver_.timeouts.page_load
+    logger.info('Browser driver is created.')
 
-logger.info('Authentication form opening...')
-driver.get("https://hh.ru/account/login?backurl=%2F")
-logger.info('Authentication form is open.')
-driver.find_element(By.CSS_SELECTOR, 'button[data-qa="expand-login-by-password"]').click()
-logger.info('Authentication form is switched.')
-driver.find_element(By.CSS_SELECTOR, 'input[data-qa="login-input-username"]').send_keys(creds_login)
-logger.info('Login input is filled.')
-driver.find_element(By.CSS_SELECTOR, 'input[data-qa="login-input-password"]').send_keys(creds_password)
-logger.info('Password input is filled.')
-driver.find_element(By.CSS_SELECTOR, 'button[data-qa="account-login-submit"]').click()
-logger.info('Authentication is complete.')
+    return driver_
+
 
 @contextmanager
 def wait_for_page_load() -> None:
     old_page = driver.find_element(By.TAG_NAME, 'html')
-    WebDriverWait(driver, timeout).until(
+    WebDriverWait(driver, driver.TIMEOUT).until(  # noqa
         EC.staleness_of(old_page)
     )
     yield
+
+
+##########################
+# Authentication section #
+##########################
+def log_in(creds_path: ty.Union[Path, str]) -> None:
+    def get_creds() -> ty.Callable:
+        with open(creds_path) as file:
+            creds_ = json.load(file)
+            get_creds.LOGIN = creds_['credentials']['login']
+            get_creds.PASSWORD = creds_['credentials']['password']
+
+        return get_creds
+
+    creds = get_creds()
+
+    logger.info('Authentication form opening...')
+    driver.set_page_load_timeout(3)
+    try:
+        driver.get("https://hh.ru/account/login?backurl=%2F")
+    except TimeoutException:
+        driver.execute_script("window.stop();")
+    driver.set_page_load_timeout(driver.DEFAULT_PAGE_LOAD_TIMEOUT)  # noqa
+    logger.info('Authentication form is open.')
+
+    WebDriverWait(driver, driver.TIMEOUT).until(  # noqa
+        EC.element_to_be_clickable((
+            By.CSS_SELECTOR,
+            'button[data-qa="expand-login-by-password"]'
+        ))
+    ).click()
+    logger.info('Authentication form is switched.')
+
+    WebDriverWait(driver, driver.TIMEOUT).until(  # noqa
+        EC.element_to_be_clickable((
+            By.CSS_SELECTOR,
+            'input[data-qa="login-input-username"]'
+        ))
+    ).send_keys(creds.LOGIN)  # noqa
+    logger.info('Login input is filled.')
+
+    WebDriverWait(driver, driver.TIMEOUT).until(  # noqa
+        EC.element_to_be_clickable((
+            By.CSS_SELECTOR,
+            'input[data-qa="login-input-password"]'
+        ))
+    ).send_keys(creds.PASSWORD)  # noqa
+    logger.info('Password input is filled.')
+
+    WebDriverWait(driver, driver.TIMEOUT).until(  # noqa
+        EC.element_to_be_clickable((
+            By.CSS_SELECTOR,
+            'button[data-qa="account-login-submit"]'
+        ))
+    ).click()
+    logger.info('Authentication is complete.')
+
 
 def find_vacancies(vacancy: str) -> None:
     logger.info('Loading main page...')
@@ -72,43 +128,91 @@ def find_vacancies(vacancy: str) -> None:
     logger.info('Page with vacancies is opened.')
 
 
-find_vacancies('Data Engineer')
+###################
+# Parsing section #
+###################
+def parse_from_page():
+    def find_or_default(soup: BeautifulSoup,
+                        selector: str,
+                        value: str = None,
+                        dafault: ty.Any = None) -> ty.Any:
+        try:
+            return soup.find(attrs={selector: value})
+        except AttributeError:
+            return dafault
 
-elements = driver.find_elements(
-    By.XPATH,
-    '//div[contains(@data-qa, "vacancy-serp__vacancy vacancy-serp__vacancy_standard") and .//*[contains(@data-qa, "vacancy-serp__vacancy_contacts")]]'
-)
-print(len(elements))
-for element in elements:
-    #####################
-    # main info section #
-    #####################
-    vacancy_soup = BeautifulSoup(element.get_attribute('innerHTML'), "lxml")
-    vacancy_address = vacancy_soup.find(attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
-    vacancy_employer = vacancy_soup.find(attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
-    vacancy_name = vacancy_soup.find(attrs={'data-qa': 'serp-item__title'}).text
-    try:
-        vacancy_work_experience = vacancy_soup.find(attrs={'data-qa': 'vacancy-serp__vacancy-work-experience'}).text
-    except (Exception,):
-        pass
-    vacancy_page_link = vacancy_soup.find(attrs={'data-qa': 'bloko-header-2'}).a['href']
-    ####################
-    # contacts section #
-    ####################
-    show_contacts_button = WebDriverWait(element, timeout).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-qa="vacancy-serp__vacancy_contacts"]'))
+    elements = driver.find_elements(
+        By.XPATH,
+        '//div[contains(@data-qa, "vacancy-serp__vacancy vacancy-serp__vacancy_standard") '
+        'and .//*[contains(@data-qa, "vacancy-serp__vacancy_contacts")]]'
     )
-    show_contacts_button.location_once_scrolled_into_view  # noqa
-    show_contacts_button.click()
-    # waiting for loading contacts form
-    contacts_form = WebDriverWait(driver, timeout).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[data-qa="drop-base"]'))
-    )
-    contacts_soup = BeautifulSoup(contacts_form.get_attribute('innerHTML'), "lxml")
-    show_contacts_button.click()
-    # webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-    print(vacancy_name, contacts_soup)
+    for element in elements:
+        # vacancy main card info
+        vacancy_soup = BeautifulSoup(element.get_attribute('innerHTML'), "lxml")
 
-    # print(vacancy_address, vacancy_employer, vacancy_name, vacancy_work_experience, vacancy_page_link)
-    # print(soup)
-    # print(vacancy_name, vacancy_page_link)
+        vacancy_address = find_or_default(
+            vacancy_soup, 'data-qa', 'vacancy-serp__vacancy-address', None
+        ).text
+        vacancy_employer = find_or_default(
+            vacancy_soup, 'data-qa', 'vacancy-serp__vacancy-employer', None
+        ).text
+        vacancy_name = find_or_default(
+            vacancy_soup, 'data-qa', 'serp-item__title', None
+        ).text
+        vacancy_work_experience = find_or_default(
+            vacancy_soup, 'data-qa', 'vacancy-serp__vacancy-work-experience', None
+        ).text
+        vacancy_page_link = find_or_default(
+            vacancy_soup, 'data-qa', 'bloko-header-2', None
+        ).a['href']
+
+        # vacancy contacts
+        show_contacts_button = WebDriverWait(element, driver.TIMEOUT).until(  # noqa
+            EC.element_to_be_clickable((
+                By.CSS_SELECTOR, 'button[data-qa="vacancy-serp__vacancy_contacts"]'
+            ))
+        )
+        show_contacts_button.location_once_scrolled_into_view  # noqa
+        show_contacts_button.click()
+        # waiting for loading contacts form
+        contacts_form = WebDriverWait(driver, driver.TIMEOUT).until(  # noqa
+            EC.presence_of_element_located((
+                By.CSS_SELECTOR, 'div[data-qa="drop-base"], div[data-qa="bloko-drop-down"]'
+            ))
+        )
+        contacts_soup = BeautifulSoup(contacts_form.get_attribute('innerHTML'), "lxml")
+
+        vacancy_hr_fio = find_or_default(
+            contacts_soup, 'data-qa', 'vacancy-contacts__fio', None
+        ).text
+        vacancy_hr_tel = ''.join(
+            find_or_default(
+                contacts_soup, 'class', 'vacancy-contacts-call-tracking__phone-number', None
+            ).text.split()
+        )
+        vacancy_hr_email = find_or_default(
+            contacts_soup, 'data-qa', 'vacancy-contacts__email', None
+        ).text
+        print(vacancy_address)
+        print(vacancy_name)
+        print(vacancy_employer)
+        print(vacancy_work_experience)
+        print(vacancy_page_link)
+        print(vacancy_hr_fio)
+        print(vacancy_hr_tel)
+        print(vacancy_hr_email)
+        print('\n'*3)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - [%(levelname)s]: %(message)s",
+        stream=sys.stdout
+    )
+    logger = logging.getLogger(__name__)
+
+    driver = create_driver(headless=True)
+    log_in('credentials.json')
+    find_vacancies('Data Engineer')
+    parse_from_page()
